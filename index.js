9bf8a9b6df2c4932777164b5c6f4c285f09ed31f
Matthew McLaren <m.mclaren@outlook.co.nz>
2022-10-05 20:50:14 +1100

    change JSON import to fetch

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-import data from "./data.json" assert {type: 'json'};
+let data
 
-const maxSpend = Math.max(...data.map(day=>day.amount))
+async function getData(){
+    const response = await fetch("./data.json")
+    data = await response.json()
+}
 
-data.forEach(day => {
-document.querySelector(".chart").innerHTML += `
-    <div class="day-container">
-        <div class="day-amount">$${day.amount}</div>
-        <div class="${day.amount===maxSpend?"day-bar highest":"day-bar"}" style="height:${day.amount/maxSpend*80}%"></div>
-        <p class="day-name">${day.day}</p>
-    </div>
-    `
-})
+function renderChart () {
+    const maxSpend = Math.max(...data.map(day=>day.amount))
+    data.forEach(day => {
+    document.querySelector(".chart").innerHTML += `
+        <div class="day-container">
+            <div class="day-amount">$${day.amount}</div>
+            <div class="${day.amount===maxSpend?"day-bar highest":"day-bar"}" style="height:${day.amount/maxSpend*80}%"></div>
+            <p class="day-name">${day.day}</p>
+        </div>
+        `
+    })
+}
 
-
+getData().then(renderChart)
